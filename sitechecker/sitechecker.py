925c6a23a072b8fdee7e9294befe53cb3ed506f4
Mikhail Sugakov <sugakov.mikhail@gmail.com>
2020-03-30 00:11:14 +0200

    Added collection of total request latency

--- sitechecker/sitechecker.py
+++ sitechecker/sitechecker.py
@@ -3,7 +3,10 @@
 import asyncio
 import signal
 import types
+from typing import NamedTuple, Optional
 
+from datetime import timedelta
+import time
 import aiohttp
 from apscheduler.events import EVENT_JOB_ERROR
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
@@ -12,19 +15,61 @@ from logzero import logger
 POLLING_SCHEDULE = types.MappingProxyType({"trigger": "interval", "seconds": 3})
 
 
+class HttpReadResult(NamedTuple):
+    """Results of site check."""
+
+    http_status: Optional[int]
+    regex_matches: Optional[bool]
+    error: Optional[Exception]
+
+    # Total time in seconds for servicing request.
+    # It includes DNS resolution, TCP connection establising, TLS/SSL handshake, sending the
+    # request, server processing and receiving the response.
+    # Note that we deviate from the problem definition which is asking to measure only "HTTP
+    # response time".
+    # Firstly,  with aiohttp it is just hard to measure only the time between request was submitted
+    # and response came back.
+    # Secondly, most service frameworks can easily capture the metric of "HTTP response time" but
+    # they can't get a latency metric as the client sees it. Total time metric could be at least as
+    # useful as HTTP response time.
+    total_time: timedelta
+
+
 async def tick() -> None:
-    await read_http("https://aiven.io/about")
-
-
-async def read_http(url: str) -> None:
     # The manual says we should not close session after every request because
     # there's a connection pool.
     # See https://docs.aiohttp.org/en/stable/client_quickstart.html#make-a-request
-    # However we'd like to measure "cold" request time including DNS resolution (TODO: verify)
-    # and establishing connection.
+    # However we'd like to measure "cold" request time including DNS resolution (TODO: verify) and
+    # establishing connection. Assumption: all monitored URLs are different sites and connection
+    # caching does not improve request latency for requests within this session.
     async with aiohttp.ClientSession() as session:
+        await read_http(session, "https://aiven.io/about")
+
+
+async def read_http(session: aiohttp.ClientSession, url: str) -> HttpReadResult:
+    logger.debug(f"Checking {url}")
+    http_status = None
+    regex_matches = None
+    error = None
+    start_counter = time.perf_counter()
+    try:
         async with session.get(url) as response:
-            logger.info(response.status)
+            end_counter = time.perf_counter()
+            logger.info(response.status)  # TODO: move me
+            http_status = response.status
+    except Exception as err:
+        end_counter = time.perf_counter()
+        logger.warning(f"Problem communicating with {url}", exc_info=err)
+        error = err
+
+    result = HttpReadResult(
+        http_status=http_status,
+        regex_matches=None,
+        error=error,
+        total_time=timedelta(seconds=end_counter - start_counter),
+    )
+    logger.info(f"{url}: {result}")
+    return result
 
 
 def setup_scheduler_application():  # noqa: WPS213
@@ -62,6 +107,7 @@ def main():
     scheduler.add_job(tick, **POLLING_SCHEDULE)
     scheduler.start()
     asyncio.get_event_loop().run_forever()
+    # TODO: catch KeyboardInterrupt and SystemExit
     logger.info("done")
 
 
--- tests/checker/test_checker.py
+++ tests/checker/test_checker.py
@@ -1,9 +1,21 @@
 import pytest
-
+import aiohttp
+from datetime import timedelta
 from sitechecker import sitechecker
 
 
 @pytest.mark.asyncio
 async def test_read_http() -> None:
-    await sitechecker.read_http("https://cnn.com")
-    assert 1 == 2 - 1
+    async with aiohttp.ClientSession() as session:
+        result = await sitechecker.read_http(session, "https://cnn.com")
+        assert (result.http_status, result.regex_matches, result.error) == (200, None, None)
+        assert result.total_time > timedelta()
+
+
+@pytest.mark.asyncio
+async def test_read_http_2() -> None:  # TODO: rename me
+    async with aiohttp.ClientSession() as session:
+        result = await sitechecker.read_http(session, "https://cnn.com123")
+        assert (result.http_status, result.regex_matches) == (None, None)
+        assert isinstance(result.error, aiohttp.client_exceptions.ClientConnectorError)
+        assert result.total_time > timedelta()
